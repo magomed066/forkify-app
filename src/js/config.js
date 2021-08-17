45d006c4a6948b883483b21391b6bf769c9bc128
Magomed Mamergov <magomedmamergov@Magomeds-MacBook-Pro.local>
2021-08-17 16:31:37 +0300

    finished the project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6646da97-d1c4-4dd2-af6e-cfe5535d7d73';
+export const MODAL_CLOSE_SEC = 2.5;
